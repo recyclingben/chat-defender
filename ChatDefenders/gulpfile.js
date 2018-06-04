77ac7d381d2238c5a94f5d0502ff71cc754cfa28
Ben <gamestraditional@gmail.com>
2018-06-04 19:51:15 -0400

    i swear if the node_modules is back i will ---

--- ChatDefenders/gulpfile.js
+++ ChatDefenders/gulpfile.js
@@ -3,15 +3,13 @@
       rename = require("gulp-rename");
 
 gulp.task('copy-modules', function () {
-    pump([
-        gulp.src('node_modules/requirejs/require.js'),
-        gulp.dest('wwwroot/js')
-    ]);
+    return gulp.src('node_modules/requirejs/require.js')
+        .pipe(gulp.dest('wwwroot/js'));
 });
 
 gulp.task('minifyJs', function () {
     
-    gulp.src(['wwwroot/js/*.js', '!wwwroot/js/*.min.js'])
+    return gulp.src(['wwwroot/js/*.js', '!wwwroot/js/*.min.js'])
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('wwwroot/js'));
