8dea731b9659b930cedaa7f0bd1984ab64165bd9
Ben <gamestraditional@gmail.com>
2018-06-05 16:53:32 -0400

    added 'added functionality to minify css files. Changed 'uglify' name for the 'gulp-uglify-es' to 'uglifyJs' to avoid confusion.

--- ChatDefenders/gulpfile.js
+++ ChatDefenders/gulpfile.js
@@ -1,6 +1,8 @@
-﻿const gulp = require("gulp"),
-      uglify = require("gulp-uglify-es").default,
-      rename = require("gulp-rename");
+﻿/// <binding ProjectOpened='watch' />
+const gulp = require("gulp"),
+        uglifyJs = require("gulp-uglify-es").default,
+        uglifyCss = require("gulp-uglifycss"),
+        rename = require("gulp-rename");
 
 gulp.task('copy-modules', function () {
     return gulp.src('node_modules/requirejs/require.js')
@@ -8,12 +10,22 @@ gulp.task('copy-modules', function () {
 });
 
 gulp.task('minifyJs', function () {
-    
+    // takes all .js files that weren't already minified
     return gulp.src(['wwwroot/js/*.js', '!wwwroot/js/*.min.js'])
-        .pipe(uglify())
+        .pipe(uglifyJs())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('wwwroot/js'));
 });
+gulp.task('minifyCss', function () {
+    // minify all js files and add '.min' suffix
+    return gulp.src(['wwwroot/css/*.css', '!wwwroot/css/*.min.css'])
+        .pipe(uglifyCss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+// watch all needed tasks
 gulp.task('watch', function () {
-    gulp.watch('wwwroot/js/*.js', ['minifyJs']); 
+    gulp.watch('wwwroot/js/*.js', ['minifyJs']);
+    gulp.watch('wwwroot/css/*.css', ['minifyCss']);
 });
